ed7c5ec2eb35c7fa237675a7c046055c79dd9476
gleb-mihalkov <gleb.mihalkov.work@yandex.ru>
2017-04-06 15:56:23 +0300

    Autocommit

--- script.js
+++ script.js
@@ -12,4 +12,19 @@ $(document).ready(function(){
       return false;
     });
   })(jQuery)
+  $(function(){
+    var $tab_nav = $('.tabs_nav .tabs_nav__item');
+    var $tab_content = $('.tabs_content .tabs_content__item');
+    $(document).on('click', $tab_nav, function(e){
+      var $self = $(e.target);
+      $tab_nav.removeClass('active');
+      $self.addClass('active');
+      var $index = $(e.target).index();
+      $tab_content.removeClass('active');
+      $tab_content.eq($index).addClass('active');
+    });
+    $('.tabs_nav .tabs_nav__item:first').addClass('active');
+    $('.tabs_content .tabs_content__item:first').addClass('active');
+    return false;
+  })
 });
